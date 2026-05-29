80ffba7766f847ef3f7983bdf999f458e525035c
Nicolás Ramos <contacto@nicolasramos.es>
2026-05-29 16:59:40 +0100

    chore: remove manual OdooClaw vendor copy

--- odooclaw/pkg/devices/source.go
+++
@@ -1,5 +0,0 @@
-package devices
-
-import "github.com/nicolasramos/odooclaw/pkg/devices/events"
-
-type EventSource = events.EventSource
--- odooclaw/scripts/download_model.py
+++
@@ -1,5 +0,0 @@
-from faster_whisper import download_model
-
-print("Downloading faster-whisper model 'tiny'...")
-download_model("tiny")
-print("Model downloaded successfully.")
